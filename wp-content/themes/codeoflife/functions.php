589cb5efd9d6022883012dcb5b3505a0c789584c
Guillermo López <guille070@gmail.com>
2021-03-20 20:59:43 +0100

    Initial commit

---
+++ wp-content/themes/codeoflife/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+	* Fichero PHP
+	*
+	* Funciones generales del tema
+	*
+	* @copyright Copyright (c) 2020 Dandy Agency
+*/
+
+// Used to define scripts and stylesheets version
+
+
+define('THEME_STYLE_VERSION', '1.0.0');
+define('THEME_TEXTDOMAIN', 'codeoflife');
+
+//include_once 'theme_core/functions/functions.php';
+//include_once 'theme_core/functions/acf_blocks.php';
+//include_once 'theme_core/functions/option_page.php';
