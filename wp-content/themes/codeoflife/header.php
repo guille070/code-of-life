84e1a1d2353e206545b2fd9dbbfd70beacd45367
Guillermo López <guille070@gmail.com>
2021-03-22 21:14:40 +0100

    Cambio en title general

--- wp-content/themes/codeoflife/header.php
+++ wp-content/themes/codeoflife/header.php
@@ -8,7 +8,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<title><?php echo bloginfo( 'name' ); ?></title>
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png">
 
     <?php wp_head(); ?>
